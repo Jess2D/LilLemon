f6d76a2e88ec1d4576e40be0128c6d654cf853e4
Jessica Silveira <jessicap_silveira@hotmail.com>
2024-02-18 17:58:05 +1300

    add footer style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-
 import './App.css';
+import logo from './assets/ux/Logo .svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-          test
-  
-       
+        <div className="header-content">
+          <img src={logo} className="App-logo" alt="Logo" />
+          <nav>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#menu">Menu</a></li>
+              <li><a href="#reservations">Reservations</a></li>
+              <li><a href="#order">Online Order</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <main>
+        <section id="home" className="about">About Us</section>
+        <section id="menu" className="specials">Our Specials</section>
+        <section id="testimonials" className="testimonials">Testimonials</section>
+      </main>
+      <footer>Test Footer</footer>
     </div>
   );
 }
