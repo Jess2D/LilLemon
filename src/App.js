c3b211636f20c0f758c7782d4128fbb9739cfb43
Jessica Silveira <jessicap_silveira@hotmail.com>
2024-02-18 18:52:22 +1300

    added about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './assets/ux/Logo .svg';
+import restaurant from './assets/images/restaurant.png'
 
 function App() {
   return (
@@ -18,7 +19,15 @@ function App() {
         </div>
       </header>
       <main>
-        <section id="home" className="about">About Us</section>
+        <section id="home" className="about">
+          <div className='text'>
+            <h1>Litle Lemon</h1>
+            <h2>New Zealand</h2>
+            <p>Delight in our exclusive dining experience where each dish tells a story of flavors and passion. Reserve your table today and embark on a culinary journey that awaits.</p>
+            <button> Reserve a Table</button>
+          </div>
+          <div className='restaurant-img'><img src={restaurant} className="restaurant" alt="Logo"></img></div>
+        </section>
         <section id="menu" className="specials">Our Specials</section>
         <section id="testimonials" className="testimonials">Testimonials</section>
       </main>
